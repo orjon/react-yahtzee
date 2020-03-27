12a46268ae3d860c4344ee240a3ac54f7b50c46b
orjon <github@orjon.com>
2020-03-27 13:11:44 +0000

    Re-roll bug fixed. Score descriptions added

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './RuleRow.css'
 
 class RuleRow extends Component {
+
   render() {
+    let {score, name, description} = this.props
+    let active = (score === undefined);
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr className={`RuleRow RuleRow-${active ? 'active' : 'disabled'}`} onClick={active ? this.props.doScore : null}>
+        <td className="RuleRow-name">{name}</td>
+        <td className="RuleRow-score">{active ? description : score}</td>
       </tr>
     )
   }
