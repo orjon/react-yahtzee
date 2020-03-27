b00cd4d46efdb5e05eef1a48db9c994f83e7ce58
orjon <github@orjon.com>
2020-03-27 15:41:55 +0000

    As complete as I want this top be

--- src/Die.js
+++ src/Die.js
@@ -10,15 +10,20 @@ class Die extends Component {
   handleClick(){
     this.props.toggleLocked(this.props.idx)
   }
+
+  // <i className='fas fa-dice-six'></i>
+
   render() {
+    const die = 'fas fa-dice-' 
+    const dieArray=['one','two','three','four','five','six']
+
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
+      <i
+        className={`Die fas fa-5x fa-dice-${dieArray[this.props.val-1]} ${this.props.locked ? 'Die-locked': ''}`}
+        
         onClick={this.handleClick}
       >
-        {this.props.val}
-      </button>
+      </i>
     );
   }
 }
